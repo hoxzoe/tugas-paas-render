861ecf1a57ac65911ae099bc5d1e9d1cf49a7665
hoxzoe <163538854+hoxzoe@users.noreply.github.com>
2025-12-05 01:44:00 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello from Render PaaS!</h1><p>Tugas PaaS berhasil online.</p>"
+
+if __name__ == "__main__":
+    app.run()
