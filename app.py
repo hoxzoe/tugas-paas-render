c9a8f3595802370e4ca829bd1f3aa8b9b9c93e4e
hoxzoe <163538854+hoxzoe@users.noreply.github.com>
2025-12-05 14:11:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from Render PaaS!</h1><p>Tugas PaaS berhasil online.</p>"
+    return "<h1>Hello from Railway PaaS!</h1><p>Tugas PaaS berhasil online.</p>"
 
 if __name__ == "__main__":
     app.run()
